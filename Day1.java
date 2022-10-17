a1ebd1faa982aaa581a2d92aac6b93f667d1fae9
masrialzaidan <masriok65@gmail.com>
2022-10-17 21:15:58 +0800

    Tipe Data

---
+++ Day1.java
@@ -0,0 +1,9 @@
+
+public class Day1 {
+    public static void main (String [] args){
+      //Deklarasi variabel
+      int value = 20;
+      //Output
+      System.out.println("Nilai dari int = " + value);
+}
+}
