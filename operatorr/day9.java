01bf2c88fd464cfc04763ee913951c1009780c14
masrialzaidan <masriok65@gmail.com>
2022-10-25 22:56:59 +0800

    if else

---
+++ operatorr/day9.java
@@ -0,0 +1,14 @@
+
+package operatorr;
+
+public class day9 {
+    public static void main(String[] args) {
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Selamat Siang");
+        } else {
+            System.out.println("Selamat Malam");
+        }
+    }
+}
+
