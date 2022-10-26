d93ae49e08bd88ac907422650172e20a84a436a3
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-10-27 00:22:37 +0800

    penggunaan boolean

---
+++ operatorr/day10.java
@@ -0,0 +1,12 @@
+package operatorr;
+
+public class day10 {
+    public static void main(String[] args) {
+        boolean A = true;
+        boolean B = false;
+        
+        System.out.println(A);
+        System.out.println(B);
+    }
+    
+}
