afb4971429366764401e751e86a8d533773bd5a4
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-10-27 22:54:35 +0800

    break

---
+++ operatorr/day11.java
@@ -0,0 +1,15 @@
+
+package operatorr;
+
+public class day11 {
+    public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 7) {
+        break;
+      }
+      System.out.println(i);
+    }
+        System.out.println("ayam");
+  }
+}
+
