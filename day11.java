1823eef447cbe9c26d64e22f7c7cd5233fa4df18
masrialzaidan <masriok65@gmail.com>
2022-10-27 22:40:45 +0800

    heyycokk11

---
+++ day11.java
@@ -0,0 +1,14 @@
+
+package operatorr;
+
+public class day11 {
+    public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 7) {
+        break;
+      }
+      System.out.println(i);
+    }  
+  }
+}
+
