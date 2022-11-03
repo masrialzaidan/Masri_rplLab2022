2b184ab8024c7f78798e1c4a0e46a554bac94cd6
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-11-03 21:35:28 +0800

    continue

---
+++ day18.java
@@ -0,0 +1,14 @@
+
+package operatorr;
+
+public class day18 {
+ public static void main(String[] args) {
+        for (int angka = 0; angka < 8; angka++) {
+            if (angka == 5) {
+                continue;
+            }
+            System.out.println("angka ke-" + angka);
+        }
+    }
+
+}
