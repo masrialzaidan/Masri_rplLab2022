b6daac35678abca4721eb18d412b8e3cc58d5e04
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-11-13 22:08:44 +0800

    menghitung bangun datar

---
+++ day28.java
@@ -0,0 +1,19 @@
+
+package operatorr;
+
+
+ 
+public class day28 {
+     //menhitung luas persegi panjang
+    public static void main(String[] args) {
+        // deklarasi variabel
+        int panjang,lebar,luas;        
+        // memberikan nilai pada variabel      
+        panjang=10;
+        lebar=6;         
+        //menentukan rumus
+        luas=panjang*lebar;    
+        //Cetak nilai 
+        System.out.println("kali: "+luas);
+    }   
+}
