c4287f6ebee37a0a4e5bdccf26f0d59a34ef6572
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-11-14 22:59:02 +0800

    Logika NOT

---
+++ Day28.java
@@ -0,0 +1,10 @@
+
+public class Day28 {
+    public static void main(String[] args) {
+        boolean a = true ;
+        boolean b = false;
+        boolean hasil = !a; //jika a bernilai true maka hasilnya akan false begitupun sebaliknya
+        System.out.println("hasil : " + hasil);
+    }
+    
+}
