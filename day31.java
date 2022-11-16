015acf83932d3112fe3fd9c25fa9659a4b7fc4c3
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-11-16 19:18:29 +0700

    menghitung nilai inputan

---
+++ day31.java
@@ -0,0 +1,24 @@
+
+package operatorr;
+
+import java.util.Scanner;
+public class day31 {
+
+ 
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  int a,b,n,total;
+  total=0;
+  Scanner keyboard=new Scanner(System.in);
+  System.out.print("Masukkan n=");
+  n=keyboard.nextInt();
+  for(a=1;a<=n;a++){
+   System.out.print("Bilangan ke"+a+"=");
+   b=keyboard.nextInt();
+   total=total+b;
+  }
+  System.out.println("Total bilangan inputan="+total);
+  keyboard.close();
+ }
+
+}
