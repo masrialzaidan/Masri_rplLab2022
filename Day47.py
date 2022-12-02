99ada9932b32b1ad51c98026e5c8eba6af61619c
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-12-02 20:01:53 +0700

    Mencari Akar

---
+++ Day47.py
@@ -0,0 +1,13 @@
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+
+num = 9
+
+# To take the input from the user
+
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+
+print('Akar dari%0.3f adalah %0.3f'%(num ,num_sqrt))
