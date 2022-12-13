80cbddf5d8ea0198fb4462f70d013a2775860fa0
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-12-13 21:08:51 +0800

    membuat tuple

---
+++ Day58.py
@@ -0,0 +1,12 @@
+#set
+matakuliah = ('Basis Data','Pemrograman web','IMK','PBO','Python')
+
+for i in matakuliah:
+    print(i)
+
+#memanggil tuple dengan indeks
+print(matakuliah[0])
+print(matakuliah[1])
+print(matakuliah[2])
+print(matakuliah[3])
+print(matakuliah[4])
