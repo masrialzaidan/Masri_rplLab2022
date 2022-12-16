374a4818a7f7d0bb414a5902dbe2c1492f58cf10
Masri_D0221036 <115980123+masrialzaidan@users.noreply.github.com>
2022-12-16 17:06:26 +0800

    dictionary

---
+++ Day61.py
@@ -0,0 +1,8 @@
+identitas = {
+    "nama" : "imbar",
+    "umur" : 19,
+    "alamat" : "Malunda"}
+
+print("identitas : ", identitas)
+print(identitas["nama"], "berumur", identitas["umur"])
+print(identitas.get("nama"))
